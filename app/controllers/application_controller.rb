d50292881de621880d7ffb1553a7aac408eda146
Pau Colomer <joebeacu@gmail.com>
2020-10-18 18:46:05 +0200

    Added 404 when page not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-	 include Pagy::Backend
+	include Pagy::Backend
+
+	rescue_from Pagy::OverflowError, :with => :page_not_found
+
+	def page_not_found
+		respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404.html", status: 404, layout: false }
+    end
+	end
 end
